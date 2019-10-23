09bc6a75d5ca040fd73b69647aa516436f639e2b
eki89 <fabian@oveleon.de>
2019-10-23 16:18:50 +0200

    Routing file removed from Plugin class.

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -33,15 +33,4 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
                 ->setReplace(['advanced-form-login']),
         ];
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
-    {
-        return $resolver
-            ->resolve(__DIR__.'/../Resources/config/routing.yml')
-            ->load(__DIR__.'/../Resources/config/routing.yml')
-            ;
-    }
 }
